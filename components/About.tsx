3ccb948f6a627844208bc9add1bb58555a744ab0
Jason Ngo <jasonsonthanhngo@gmail.com>
2023-10-12 08:40:31 -0700

    fix flex

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ type Props = {};
 function About({}: Props) {
   return (
     <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[10px] text-2xl md:text-6xl text-red-500 font-semibold ">
+      <h3 className="absolute top-24 uppercase tracking-[10px] text-xl sm:text-2xl md:text-4xl text-red-500 font-semibold ">
         💞💙Love💙💞
       </h3>
 
@@ -16,11 +16,13 @@ function About({}: Props) {
         initial={{ x: +200, opacity: 0 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, x: 0 }}
-        className="-mb-20 md:mb-0 flex-shrink-0 rounded-full w-[235px] h-[138.5px] md:w-[570px] md:h-[277px]"
+        className="-mb-20 w-[235px] h-[138.5px] md:mb-0 flex-shrink-0 rounded-full md:w-[470px] md:h-[277px] lg:w-[570px]"
       />
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">Happy Anniversary Babi</h4>
-        <p className="text-lg text-pink-900">
+        <h4 className="text-xl md:text-2xl font-semibold">
+          Happy Anniversary Babi
+        </h4>
+        <p className="text-sm text-pink-900 md:text-lg mx-auto max-w-md px-4">
           Here&#39;s to a year of us - of the smiles, the laughter, and the
           moments that define our story. From that very first smile, you&#39;ve
           filled my days with warmth and my heart with love. Here&#39;s to the
